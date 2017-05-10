f7217be2f189426637de3fb9c8be68b7d7de3434
ravichandra522 <ravichandra522@gmail.com>
2017-05-10 13:17:33 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Quiz from './Quiz';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        
+        <h1>RANDOMIZER QUIZ</h1>
+        
+        <Quiz/>
+      </div>
+    );
+  }
+}
+
+export default App;
